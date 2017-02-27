9ec3fe11fa6d1af980c4b55cda8aa31d585332ef
DOCTOR <mail.yashlala@gmail.com>
2017-02-28 02:14:42 +0530

    ADDED SELECTION FEATURE

--- app/src/main/java/com/example/alienware/hidetoolbaronscrollrecycler/TheAdapter.java
+++ app/src/main/java/com/example/alienware/hidetoolbaronscrollrecycler/TheAdapter.java
@@ -1,5 +1,6 @@
 package com.example.alienware.hidetoolbaronscrollrecycler;
 
+import android.graphics.Color;
 import android.support.design.widget.CoordinatorLayout;
 import android.support.design.widget.Snackbar;
 import android.support.v7.widget.RecyclerView;
@@ -8,9 +9,12 @@ import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 import org.json.JSONException;
 import org.json.JSONObject;
+
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -21,32 +25,47 @@ public class TheAdapter extends RecyclerView.Adapter<TheAdapter.TheViewHolder> {
 
     List<JSONObject> jsonObjectList;
     CoordinatorLayout coordinatorLayout;
+    ArrayList<Integer> selectedvalues = new ArrayList();
+    int selectedcolor = Color.parseColor("#960000");
 
 
     //Class for View Holder, layout stuff for each card
     public class TheViewHolder extends RecyclerView.ViewHolder{
         TextView name,user_name;
         Button add;
-        public TheViewHolder(View itemView) {
+        RelativeLayout relativeLayout;
+        public TheViewHolder(final View itemView) {
             super(itemView);
-            coordinatorLayout = (CoordinatorLayout)itemView.findViewById(R.id.frag_coordinator);
+            relativeLayout = (RelativeLayout)itemView.findViewById(R.id.card);
             name = (TextView)itemView.findViewById(R.id.name);
             user_name = (TextView)itemView.findViewById(R.id.user_name);
-            add = (Button)itemView.findViewById(R.id.add);
+            add = (Button)itemView.findViewById(R.id.button);
+
+
             itemView.setOnTouchListener(new View.OnTouchListener() {
                 @Override
                 public boolean onTouch(View view, MotionEvent motionEvent) {
                     switch (motionEvent.getAction()){
-                        case MotionEvent.ACTION_DOWN:
-                            switch (view.getId()){
-                                case R.id.add:
-                                    //Snackbar.make(coordinatorLayout,"ADD->"+jsonObjectList.get(getAdapterPosition()).toString(),Snackbar.LENGTH_SHORT).show();
-                                    return true;
-                                default:
-                                    //Snackbar.make(coordinatorLayout,jsonObjectList.get(getAdapterPosition()).toString(),Snackbar.LENGTH_SHORT).show();
-                                    return true;
-                            }
                         case MotionEvent.ACTION_UP:
+                            //switch (view.getId()){
+                              //  case R.id.button:
+                                //    Snackbar.make(itemView,"ADD->"+jsonObjectList.get(getAdapterPosition()).toString(),Snackbar.LENGTH_SHORT).show();
+                                  //  break;
+                                //default:
+                            if(selectedvalues.contains(getAdapterPosition())){
+                                relativeLayout.setBackgroundColor(Color.TRANSPARENT);
+                                selectedvalues.remove((Integer) getAdapterPosition());
+                            }else{
+                                selectedvalues.add(getAdapterPosition());
+                                relativeLayout.setBackgroundColor(selectedcolor);
+                            }
+
+
+                                    Snackbar.make(itemView,jsonObjectList.get(getAdapterPosition()).toString(),Snackbar.LENGTH_SHORT).show();
+                                  //   break;
+                            //}
+                            return true;
+                        case MotionEvent.ACTION_DOWN:
                             return true;
                     }
                     return true;
@@ -66,6 +85,13 @@ public class TheAdapter extends RecyclerView.Adapter<TheAdapter.TheViewHolder> {
 
     @Override
     public void onBindViewHolder(TheViewHolder holder, int position) {
+
+        if(selectedvalues.contains(position)){
+         holder.relativeLayout.setBackgroundColor(selectedcolor);
+        }else {
+            holder.relativeLayout.setBackgroundColor(Color.TRANSPARENT);
+        }
+
         JSONObject jsonObject = jsonObjectList.get(position);
         try {
             holder.user_name.setText(jsonObject.getString("user_name"));
